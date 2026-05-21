c630011361c7f3affab5944c7b704e0409eb33b9
Joe P Shoulak <joepshoulak@gmail.com>
2026-05-21 17:43:44 -0500

    polish and notes

--- Orbit.py
+++ Orbit.py
@@ -1,14 +1,16 @@
 class Orbit:
-    def __init__(self, sma, ecc, inc, argP, ttp, lan):
-        self.sma = sma # semimajor axis in km
-        self.ecc = ecc # eccentricity
-        self.inc = inc # inclination, in desgrees, to parent body's equator, not invariable plane 
-                       # TODO: Confirm that's the right choice
-        self.argP = argP # argument of periapsis, in degrees
-        self.ttp = ttp # time to periapsis passage (unlisted for earth on wiki at the moment)
-        self.lan = lan # in degrees, usually from the vernal equinox
-                       # TODO: Confirm that's correct
-        # Other inmportant properties that can be used in part of the signature to define the orbit
+    # TODO: In general, double check these parameters and their units
+    def __init__(self, semimajor_axis, eccentricity, inclination, argument_of_periapsis, time_to_periapsis, longitude_of_ascending_node):
+        self.sma = semimajor_axis               # in km
+        self.ecc = eccentricity                 # between 0 and 1 where 0 is a circle
+        self.inc = inclination                  # in degrees, to parent body's equator, not invariable plane 
+                                                # TODO: Confirm that's the right choice
+        self.argp = argument_of_periapsis       # in degrees
+        self.ttp = time_to_periapsis            # TODO: Determine correct units
+        self.lan = longitude_of_ascending_node      # in degrees, usually from the vernal equinox
+                                                # TODO: Confirm that's correct
+
+        # Other important properties that can be used in part of the signature to define the orbit
         # or derived from the above properties:
             # Apopasis distance
             # Periapsis distance
@@ -16,6 +18,6 @@ class Orbit:
             # True Anomaly
 
     def __str__(self):
-        return f"Orbit(sma={self.sma}, ecc={self.ecc}, inc={self.inc}, argP={self.argP}, ttp={self.ttp}, lan={self.lan})"
+        return f"Orbit(sma={self.sma}, ecc={self.ecc}, inc={self.inc}, argP={self.argp}, ttp={self.ttp}, lan={self.lan})"
     def __repr__(self):
         return self.__str__() # TODO: Do I need a different __repr__?
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from Orbit import Orbit
 
 # Earth will next arrive at it's perihelion on January 2nd, 2027 at 02:32 UTC
 # https://in-the-sky.org/news.php?id=20270103_07_100
+# TODO: Get this from the Horizons API instead
 
 earth_perihelion_date = datetime(2027, 1, 2, 2, 32, tzinfo=timezone.utc)
 ttp = (earth_perihelion_date - datetime.now(timezone.utc)).total_seconds()
